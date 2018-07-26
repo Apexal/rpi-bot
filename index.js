559ef88b557e45c0c40d2934a14f027b755fff1e
Frank Matranga <thefrankmatranga@gmail.com>
2018-07-25 21:13:46 -0400

    Dynamic event handlers

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+const fs = require('fs');
+
 const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// Holds all event handlers
+client.events = new Discord.Collection();
+const eventFiles = fs
+  .readdirSync('./events')
+  .filter(file => file.endsWith('.js'))
+  .map(file => file.slice(0, -3)); // Remove '.js'
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+for (const file of eventFiles) {
+  const handler = require(`./events/${file}`);
+  client.events.set(file, handler);
+
+  client.on(file, () => handler(client));
+}
+
+client.on('message', msg => {});
 
 client.login(config.token);
