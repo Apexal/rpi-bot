b1a79767d399569e6c8c3756409ea727992001ea
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-20 20:38:56 -0400

    Command system

---
+++ commands/dorm.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'dorm',
+  uses: {
+    'View a users dorm info': '@User',
+    'Set your own dorm info': 'Burdett-100-A1'
+  },
+  run(message, args) {}
+};
--- index.js
+++ index.js
@@ -16,8 +16,62 @@ for (const file of eventFiles) {
   client.events.set(file, handler);
 
   client.on(file, () => handler(client));
+  console.log(`[Loaded ${eventFiles.length} event handlers]`);
 }
 
-client.on('message', msg => {});
+// Load all commands
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'))
+  .map(file => file.slice(0, -3));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(file, command);
+  console.log(`[Loaded ${commandFiles.length} commands]`);
+}
+
+// Command handling
+client.on('message', msg => {
+  if (msg.content.startsWith(config.prefix)) {
+    const parts = msg.content.split(' ');
+    let [commandName, ...args] = parts;
+    commandName = commandName.substring(1);
+
+    // Help command
+    if (commandName === 'help' && args.length > 0) {
+      if (!client.commands.has(args[0]))
+        return msg.channel.send('No such command exists!');
+
+      const command = client.commands.get(args[0]);
+      const uses = Object.keys(command.uses)
+        .map(
+          // e.g. 'View a users dorm info: `!dorm @User`'
+          desc =>
+            desc +
+            ': `' +
+            config.prefix +
+            args[0] +
+            ' ' +
+            command.uses[desc] +
+            '`'
+        )
+        .join('\n');
+
+      msg.channel.send(
+        '**Command**: `' + config.prefix + args[0] + '`\n' + uses
+      );
+    } else if (client.commands.has(commandName)) {
+      // Execute command
+      console.log(
+        `[Executing command '${commandName}' for @${msg.author.tag}] '${
+          msg.content
+        }'`
+      );
+      client.commands.get(commandName).run(msg, args);
+    }
+  }
+});
 
 client.login(config.token);
